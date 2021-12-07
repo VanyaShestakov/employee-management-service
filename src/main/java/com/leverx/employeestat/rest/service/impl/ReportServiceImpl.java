31a2d010cc01af0547d0f4eb9fa067589a6da301
VanyaShestakov <vanka1108@gmail.com>
2021-12-07 16:56:39 +0300

    Moved writing file into response from ReportService to ReportController

--- src/main/java/com/leverx/employeestat/rest/service/impl/ReportServiceImpl.java
+++ src/main/java/com/leverx/employeestat/rest/service/impl/ReportServiceImpl.java
@@ -17,8 +17,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -32,8 +30,6 @@ import java.util.stream.Collectors;
 @Service
 public class ReportServiceImpl implements ReportService {
 
-    private final static String CONTENT_TYPE = "application/octet-stream";
-    private final static String HEADER_INFO = "Content-Disposition";
     private final static String FIRST_NAME_COL = "First Name";
     private final static String LAST_NAME_COL = "Last Name";
     private final static String DEPARTMENT_COL = "Department";
@@ -87,9 +83,7 @@ public class ReportServiceImpl implements ReportService {
 
     @Override
     @Transactional
-    public void exportOccupationReport(HttpServletResponse response) {
-        response.setContentType(CONTENT_TYPE);
-        response.setHeader(HEADER_INFO, "attachment; filename=Employees.xlsx");
+    public XSSFWorkbook exportOccupationReport() {
         createTableHeaders(FIRST_NAME_COL, LAST_NAME_COL, DEPARTMENT_COL, PROJECT_OCCUPATION_COL);
         List<Work> works = workRepository.findAll();
         for (int i = 0; i < works.size(); i++) {
@@ -112,21 +106,14 @@ public class ReportServiceImpl implements ReportService {
             Cell occupation = row.createCell(3);
             occupation.setCellValue(String.format("%s - (%s - %s)", project.getName(), work.getPositionStartDate(), work.getPositionEndDate()));
         }
-        try (ServletOutputStream outputStream = response.getOutputStream()) {
-            workbook.write(outputStream);
-        } catch (IOException e) {
-            throw new ReportWritingException("Failed to write report", e);
-        }
+        return workbook;
     }
 
     @Override
-    public void exportLastGeneratedReport(HttpServletResponse response) {
+    public XSSFWorkbook exportLastGeneratedReport() {
         File latestReport = getLatestReport();
-        response.setContentType(CONTENT_TYPE);
-        response.setHeader(HEADER_INFO, String.format("attachment; filename=%s", latestReport.getName()));
-        try (ServletOutputStream outputStream = response.getOutputStream()) {
-            workbook = new XSSFWorkbook(latestReport);
-            workbook.write(outputStream);
+        try {
+            return new XSSFWorkbook(latestReport);
         } catch (IOException | InvalidFormatException e) {
             throw new ReportWritingException("Failed to write report", e);
         }
